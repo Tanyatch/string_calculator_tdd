8f8c9d64d14616b347018ce98a887b46444aba26
Tanya Omar <tanyaomar17@gmail.com>
2026-01-07 21:45:58 +0530

    GREEN:Implement add method returning 0

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,5 +1,5 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class StringCalculator {
+   int add(String numbers) {
+    return 0;
+  }
 }
